6efcea75333fd38d01291ec6148ea108f82e928b
jtechico <jeff.techico@gmail.com>
2023-11-06 14:18:21 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,8 +51,8 @@ try:
     # streamlit.dataframe(fruityvice_normalized)
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-# except URLError as e:
-    # streamlit.error()
+except URLError as e:
+    streamlit.error()
 streamlit.stop()
 
 # import snowflake.connector
